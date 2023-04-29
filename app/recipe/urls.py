1b7a0a9f591de800ca37217793353f2500cdbe91
中島哲史 <nakashi120@nakajimaayanonoMacBook-Air.local>
2023-04-30 00:29:27 +0700

    Added additional tests for recipe API

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -8,8 +8,7 @@ from django.urls import (
 
 from rest_framework.routers import DefaultRouter
 
-from . import views
-# from recipe import views
+from recipe import views
 
 router = DefaultRouter()
 router.register('recipes', views.RecipeViewSet)
