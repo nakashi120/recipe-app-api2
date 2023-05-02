b973cd2162e32471cf278567f15a724d060ecef2
中島哲史 <nakashi120@nakajimaayanonoMacBook-Air.local>
2023-05-03 00:06:16 +0700

    Revised

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -261,7 +261,7 @@ class PrivateRecipeApiTests(TestCase):
         """Test creating tag when updating a recipe."""
         recipe = create_recipe(user=self.user)
 
-        payload = {'tags': {'name': 'Lunch'}}
+        payload = {'tags': [{'name': 'Lunch'}]}
         url = detail_url(recipe.id)
         res = self.client.patch(url, payload, format='json')
 
